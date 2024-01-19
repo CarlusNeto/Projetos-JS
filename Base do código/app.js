78927252bd69d7f0257e6f359b23e2f17c30dc21
Carlos Neto <carlosaugnet@gmail.com>
2024-01-19 15:30:25 -0300

    JS

---
+++ Base do código/app.js
@@ -0,0 +1,11 @@
+alert('Brota no jogo do cleitin calabreso!');
+let numeroSecreto = 2;
+console.log(numeroSecreto)
+let chute = prompt('Escolha teu número mala, entre 1 e 500'); //o let dessa linha está sendo usado para deixar armazenado o número que a pessoa digitar
+
+// se o chute for igual ao número secreto
+if (chute == numeroSecreto) {
+    alert(`Boa Bahia, o número é ${numeroSecreto}`);
+} else {
+    alert('c é burro em perna')
+}
