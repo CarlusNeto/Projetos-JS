7cf4460e539fb3e3a35ab3f86ca7462ae0c06d82
Carlos Neto <carlosaugnet@gmail.com>
2024-01-19 15:11:29 -0300

    Update app.js

--- JavaScript - Alura/logica-js-projeto_inicial/app.js
+++ JavaScript - Alura/logica-js-projeto_inicial/app.js
@@ -7,6 +7,9 @@ let chute = prompt('Escolha teu número mala, entre 1 e 500'); //o let dessa lin
 if (chute == numeroSecreto) {
     alert(`Boa Bahia, o número é ${numeroSecreto}`);
 } else {
-    alert('c é burro em perna')
+    if(chute > numeroSecreto) {
+        alert(`O Número Secreto é Maior que ${chute}`)
+    } else {
+        alert(`O Número Secreto é Maior que ${chute}`)
+    }
 }
-
