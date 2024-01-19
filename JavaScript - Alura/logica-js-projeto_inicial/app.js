40ea90745439ef87ac3fcdfac5f1b1c1402d152a
Carlos Neto <carlosaugnet@gmail.com>
2024-01-19 11:06:00 -0300

    JS

---
+++ JavaScript - Alura/logica-js-projeto_inicial/app.js
@@ -0,0 +1,20 @@
+diaDaSemana = prompt('Qual dia da semana é?')
+
+if (diaDaSemana == 'Sábado') {
+    alert('Bom Final de Semana!');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom Final de Semana!');
+} else if (diaDaSemana == 'Segunda'){
+    alert('Boa Semana!');
+} else if (diaDaSemana == 'Terça'){
+    alert('Boa Semana!');
+} else if (diaDaSemana == 'Quarta'){
+    alert('Boa Semana!');
+} else if (diaDaSemana == 'Quinta'){
+    alert('Boa Semana!');
+} else if (diaDaSemana == 'Sexta'){
+    alert('Sextou carialiooooo');
+}else {
+    alert('Ta errado isso ai Jamelão');
+}
+
